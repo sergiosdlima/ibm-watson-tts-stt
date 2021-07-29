916034df7d98b0616dec949936737165f5cfb9ec
Sérgio Lima <sergiosdlima@gmail.com>
2021-07-28 23:43:08 -0300

    Implementa a função speech to text

--- app.py
+++ app.py
@@ -3,25 +3,30 @@
 import os
 from os.path import join, dirname
 import logging
-from flask import Flask, render_template, request, jsonify, send_file
-from ibm_watson import TextToSpeechV1
+from flask import Flask, render_template, request, jsonify, send_file, Response
+from ibm_watson import TextToSpeechV1, SpeechToTextV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 app = Flask(__name__)
 app.debug = 'DEBUG' in os.environ
 
-API_KEY = os.environ['API_KEY']
+TTS_API_KEY = os.environ['TTS_API_KEY']
 TTS_SERVICE_URL = os.environ['TTS_SERVICE_URL']
+STT_API_KEY = os.environ['STT_API_KEY']
+STT_SERVICE_URL = os.environ['STT_SERVICE_URL']
 
 file_path = join(dirname(__file__), 'audio.mp3')
 
 class IBMWatson(object):
     def __init__(self):
-        authenticator = IAMAuthenticator(API_KEY)
         self.tts = TextToSpeechV1(
-            authenticator=authenticator
+            authenticator=IAMAuthenticator(TTS_API_KEY)
         )
         self.tts.set_service_url(TTS_SERVICE_URL)
+        self.stt = SpeechToTextV1(
+            authenticator=IAMAuthenticator(STT_API_KEY)
+        )
+        self.stt.set_service_url(STT_SERVICE_URL)
     
     def synthesize(self, text):
         if (os.path.exists(file_path)):
@@ -35,6 +40,17 @@ class IBMWatson(object):
             ).get_result().content
             audio_file.write(content)
 
+    def recognize(self, audio):
+        with open(audio, 'rb') as audio_file:
+            speech_recognition_results = self.stt.recognize(
+                audio=audio_file,
+                content_type='audio/webm',
+                model='pt-BR_BroadbandModel',
+                # max_alternatives=3
+            ).get_result()
+        # print(json.dumps(speech_recognition_results, indent=2, ensure_ascii=False))
+        return speech_recognition_results
+
 watson = IBMWatson()
 
 @app.route('/')
@@ -46,10 +62,21 @@ def tts():
     text = request.form['text']
     if text:
         watson.synthesize(text)
-        return jsonify({'output': text, 'audio_url': '/audio'})
+        return jsonify({'success': True, 'audio_url': '/audio'})
     else:
-        return jsonify({'error': 'Missing data!'})
+        return jsonify({'success': False, 'error': 'Dados ausentes!'})
 
 @app.route('/audio', methods=['GET'])
 def get_audio():
     return send_file(file_path)
+
+@app.route('/stt', methods=['POST'])
+def stt():
+    audio = request.files['blob']
+    if audio:
+        filename = 'fala.webm'
+        audio.save(filename)
+        result = watson.recognize(filename)
+        return jsonify({'success': True, 'result': result})
+    else:
+        return jsonify({'success': False, 'error': 'Não foi possível transcrever a fala.'})
--- static/js/application.js
+++ static/js/application.js
@@ -1,23 +1,87 @@
-const statusMsg = function(msg) {
-  $('#audio-log').text(msg);
+// UTILS
+const printMsg = function(el, msg) {
+  el.text(msg);
 }
 
+// TEXT TO SPEECH
 $("#input-form").on("submit", function(event) {
   event.preventDefault();
   const text = $("#input-text")[0].value;
-  statusMsg('Carregando...');
-  $.ajax({
-    data: {
-      text: text
-    },
-    type: 'POST',
-    url: '/tts'
-  }).done(function(data) {
-    statusMsg('Tocando áudio...');
-    const audio = new Audio(data.audio_url);
-    audio.play();
-    audio.onended = function() {
-      statusMsg('');
-    };
-  });
+  const textLog = $('#text-log');
+  if (text) {
+    printMsg(textLog, 'Enviando texto...');
+    $.ajax({
+      data: {
+        text: text
+      },
+      type: 'POST',
+      url: '/tts'
+    }).done(function(data) {
+      printMsg(textLog, 'Tocando áudio...');
+      const audio = new Audio(data.audio_url);
+      audio.play();
+      audio.onended = function() {
+        printMsg(textLog, '');
+      };
+    });
+  }
 });
+
+// SPEECH TO TEXT
+let log = console.log.bind(console);
+let mediaRecorder;
+const startButton = document.getElementById('start-speech');
+const stopButton = document.getElementById('stop-speech');
+const audioLog = $('#audio-log');
+
+startButton.addEventListener('click', function() {
+  audioLog.val('Escutando...');
+  mediaRecorder.start();
+});
+
+stopButton.addEventListener('click', function() {
+  mediaRecorder.stop();
+});
+
+const handleSuccess = function(stream) {
+  const options = {mimeType: 'audio/webm'};
+  let recordedChunks = [];
+  mediaRecorder = new MediaRecorder(stream, options);
+
+  mediaRecorder.addEventListener('start', function() {
+    if (recordedChunks.length) {
+      recordedChunks = [];
+    }
+  });
+
+  mediaRecorder.addEventListener('dataavailable', function(e) {
+    audioLog.val('Processando...');
+    if (e.data.size > 0) {
+      recordedChunks.push(e.data);
+    }
+  });
+
+  mediaRecorder.addEventListener('stop', function() {
+    const audioBlob = new Blob(recordedChunks);
+    let fd = new FormData();
+    fd.append('blob', audioBlob);
+    $.ajax({
+      method: 'POST',
+      url: '/stt',
+      data: fd,
+      processData: false,
+      contentType: false
+    }).done((data) => {
+      audioLog.val('');
+      textResult = '';
+      for (let i = 0; i < data.result.results.length; i++) {
+        textResult += data.result.results[i].alternatives[0].transcript + ' ';
+      }
+      audioLog.val(textResult);
+    });
+  });
+};
+// Acessa o microfone de forma interativa
+navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+  .then(handleSuccess)
+  .catch(log);
